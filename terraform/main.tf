ac51ab956187a50c1a712fc01799fde7428f4cbe
Anish Sapkota <sapkotaanish000@gmail.com>
2024-06-06 16:22:05 +0545

    fixed ecr creation terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,6 @@ module "ecr" {
   count = length(var.ecr_name)
   source = "terraform-aws-modules/ecr/aws"
 
-  repository_name = var.ecr_name[index].value
+  repository_name = var.ecr_name[count.index]
   create_lifecycle_policy	= false
 }

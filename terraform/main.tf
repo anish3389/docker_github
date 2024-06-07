6a48568e41c19272f31e1f1e3de0297335ed2141
Anish Sapkota <sapkotaanish000@gmail.com>
2024-06-07 10:07:29 +0545

    added ec2 instance in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,27 @@ module "ecr" {
   create_lifecycle_policy	= false
   repository_image_tag_mutability = "MUTABLE"
 }
+
+module "ec2" {
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "5.5.0"
+  instance_type               = var.instance_type
+  ami                         = var.ami_id
+  monitoring                  = var.monitoring
+  associate_public_ip_address = var.associate_public_ip_address
+  vpc_security_group_ids      = var.sg_ids
+  subnet_id                   = var.subnet_id
+  create_iam_instance_profile = var.create_iam_instance_profile
+  iam_role_description        = "SSM Role for accessing EC2 instance"
+  user_data_replace_on_change = true
+
+  iam_role_policies = {
+    SSM = var.ssm_policy
+    ECR = var.ecr_policy
+  }
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
-# Argument Reference for the resources
+# Argument Reference for the project
 ################################################################################
 
 variable "region" {
@@ -31,9 +31,74 @@ variable "silo" {
   default     = ""
 }
 
+# Argument Reference for ECR
+################################################################################
 
 variable "ecr_name" {
   description = "List of ECRs"
   type = list(string)
   default = []
+}
+
+# Argument Reference for EC2
+################################################################################
+
+variable "ami_id" {
+  description = "AMI of the server (Default is Ubuntu 22.04)"
+  type        = string
+  default     = "ami-012485deee5681dc0"
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+  type        = string
+  default     = "t2.nano"
+}
+
+variable "sg_ids" {
+  description = "Security Group ID for the instance"
+  type        = list(string)
+  default     = []
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where instance will be placed"
+  type        = string
+  default     = ""
+}
+
+variable "monitoring" {
+  description = "Whether to enable monitoring for the server"
+  type        = bool
+  default     = false
+}
+
+variable "associate_public_ip_address" {
+  description = "Whether to associate public ip to the server"
+  type        = bool
+  default     = false
+}
+
+variable "create_iam_instance_profile" {
+  description = "Whether to create IAM Instance profile and attach to the server"
+  type        = bool
+  default     = false
+}
+
+variable "ssm_policy" {
+  description = "Attach SSM policy to the server"
+  type        = string
+  default     = ""
+}
+
+variable "ecr_policy" {
+  description = "Attach SSM policy to the server"
+  type        = string
+  default     = ""
+}
+
+variable "instance_name" {
+  description = "Name of the instance"
+  type        = string
+  default     = ""
 }
